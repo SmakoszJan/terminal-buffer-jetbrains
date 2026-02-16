2c7ea86e0a5152fe635f7f1aac6e4f06ba361e76
SmakoszJan <margos.michal@gmail.com>
2026-02-16 21:18:43 +0100

    Extracted attributes from cell & formatting

--- src/main/kotlin/TerminalBuffer.kt
+++ src/main/kotlin/TerminalBuffer.kt
@@ -4,7 +4,13 @@ enum class Color {
 
 data class Style(val bold: Boolean = false, val italic: Boolean = false, val underline: Boolean = false)
 
-data class Cell(val content: Char? = null, val fgColor: Color = Color.DEFAULT, val bgColor: Color = Color.DEFAULT, val style: Style = Style())
+data class Attributes(
+    val fgColor: Color = Color.DEFAULT,
+    val bgColor: Color = Color.DEFAULT,
+    val style: Style = Style()
+)
+
+data class Cell(val content: Char? = null, val attributes: Attributes = Attributes())
 
 // Negative line number refers to scrollback when reading
 data class Position(val col: Int, val ln: Int)
@@ -13,7 +19,9 @@ private class RectBuffer(val width: Int, val height: Int) {
     private val buffer = RollingBuffer(Array(width * height) { Cell() })
 
     operator fun get(pos: Position) = buffer[pos.ln * width + pos.col]
-    operator fun set(pos: Position, cell: Cell) { buffer[pos.ln * width + pos.col] = cell }
+    operator fun set(pos: Position, cell: Cell) {
+        buffer[pos.ln * width + pos.col] = cell
+    }
 }
 
 class TerminalBuffer(val width: Int, val height: Int, val scrollback: Int) {
@@ -21,21 +29,33 @@ class TerminalBuffer(val width: Int, val height: Int, val scrollback: Int) {
     private val screen = RectBuffer(width, height)
     private val scrollbackBuffer = RectBuffer(width, scrollback)
     var cursor = Position(0, 0)
-        set(value) { field = Position(value.col.coerceIn(0..<width), value.ln.coerceIn(0..<height)) }
+        set(value) {
+            field = Position(value.col.coerceIn(0..<width), value.ln.coerceIn(0..<height))
+        }
 
     operator fun get(pos: Position) = if (pos.ln >= 0) {
         screen[pos]
     } else {
         scrollbackBuffer[Position(pos.col, scrollback + pos.ln)]
     }
+
     operator fun get(col: Int, ln: Int) = get(Position(col, ln))
 
     // NOTE: Horizontal cursor movements don't wrap to other lines
     fun cursorLeft(by: Int = 1) {
         cursor = Position(cursor.col - by, cursor.ln)
     }
+
     // NOTE: Horizontal cursor movements don't wrap to other lines
-    fun cursorRight(by: Int = 1) { cursor = Position(cursor.col + by, cursor.ln) }
-    fun cursorUp(by: Int = 1) { cursor = Position(cursor.col, cursor.ln - by) }
-    fun cursorDown(by: Int = 1) { cursor = Position(cursor.col, cursor.ln + by) }
+    fun cursorRight(by: Int = 1) {
+        cursor = Position(cursor.col + by, cursor.ln)
+    }
+
+    fun cursorUp(by: Int = 1) {
+        cursor = Position(cursor.col, cursor.ln - by)
+    }
+
+    fun cursorDown(by: Int = 1) {
+        cursor = Position(cursor.col, cursor.ln + by)
+    }
 }
