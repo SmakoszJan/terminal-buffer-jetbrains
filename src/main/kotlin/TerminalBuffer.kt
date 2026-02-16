abe3b6b56f2d7104715a7cb8dad7f93a69e07edb
SmakoszJan <margos.michal@gmail.com>
2026-02-16 20:01:37 +0100

    Empty cells now have null as content instead of spaces

--- src/main/kotlin/TerminalBuffer.kt
+++ src/main/kotlin/TerminalBuffer.kt
@@ -4,10 +4,10 @@ enum class Color {
 
 data class Style(val bold: Boolean = false, val italic: Boolean = false, val underline: Boolean = false)
 
-data class Cell(val content: Char, val fgColor: Color, val bgColor: Color, val style: Style)
+data class Cell(val content: Char?, val fgColor: Color, val bgColor: Color, val style: Style)
 
 class TerminalBuffer(val width: Int, val height: Int, scrollback: Int) {
     // Arrays used for performance
-    private val screen = Array(width * height) { Cell(' ', Color.DEFAULT, Color.DEFAULT, Style()) }
-    private val scrollback = Array<String>(scrollback) { "" }
+    private val screen = Array(width * height) { Cell(null, Color.DEFAULT, Color.DEFAULT, Style()) }
+    private val scrollback = Array(scrollback) { "" }
 }
