ecbe72395df7d5107eb1dad2eb1128bd4898091e
SmakoszJan <margos.michal@gmail.com>
2026-02-16 19:38:59 +0100

    init

--- src/main/kotlin/TerminalBuffer.kt
+++ src/main/kotlin/TerminalBuffer.kt
@@ -1,4 +1,13 @@
+enum class Color {
+    DEFAULT, BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
+}
+
+data class Style(val bold: Boolean = false, val italic: Boolean = false, val underline: Boolean = false)
+
+data class Cell(val content: Char, val fgColor: Color, val bgColor: Color, val style: Style)
+
 class TerminalBuffer(val width: Int, val height: Int, scrollback: Int) {
-    private val buffer = CharArray(width * height) { ' ' }
-    private val scrollback = CharArray(scrollback) { ' ' }
+    // Arrays used for performance
+    private val screen = Array(width * height) { Cell(' ', Color.DEFAULT, Color.DEFAULT, Style()) }
+    private val scrollback = Array<String>(scrollback) { "" }
 }
